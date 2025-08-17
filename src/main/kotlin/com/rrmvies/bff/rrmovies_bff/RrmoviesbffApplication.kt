70101165614db1d9802759828ae989a86d1311c9
rodolfopp <rodolfo.ruiz@picpay.com>
2025-08-17 01:17:16 -0300

    adicionado spring do initializer

---
+++ src/main/kotlin/com/rrmvies/bff/rrmovies_bff/RrmoviesbffApplication.kt
@@ -0,0 +1,11 @@
+package com.rrmvies.bff.rrmovies_bff
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RrmoviesbffApplication
+
+fun main(args: Array<String>) {
+	runApplication<RrmoviesbffApplication>(*args)
+}
---
+++ src/test/kotlin/com/rrmvies/bff/rrmovies_bff/RrmoviesbffApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rrmvies.bff.rrmovies_bff
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RrmoviesbffApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
